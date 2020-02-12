5e1ef0396ad0538801f52a76ba29550e4baa8145
Lucas Augusto Cavenaghi <cavenaghi@ibm.com>
2020-02-11 23:18:28 -0300

    starts css implementation

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './Global.css'
+import './App.css'
+
+// Component - isolated / reutilizable code block that doesn't interfere in others
+// Property - information that is passed through objects - example <Header title="Hello world1"/> (title is the property)
+// State - information kept by the component (imutability)
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Github Username</label>
+            <input name="github_username" id="github_username" required/>
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="techs">Techs</label>
+            <input name="techs" id="techs" required/>
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required/>
+            </div>
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required/>
+            </div>
+          </div>
+
+          <button>Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+
+      </main>
     </div>
   );
 }
